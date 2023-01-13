159405f6303fb56fe79fe9672fe513caf22ba4d2
Katine <gmartinelli.osef@gmail.com>
2023-01-13 13:45:11 -0300

    Create MOD5_U4

---
+++ MOD5_U4/models/usuariosModel.js
@@ -0,0 +1,15 @@
+var pool = require("./bd"); //Llamando datos BD
+var md5 = require("md5");
+
+async function getUserByUsernameAndPassword(user, password) {
+  try {
+    var query =
+      "SELECT * FROM usuarios WHERE user = ? and password = ? limit 1";
+    var rows = await pool.query(query, [user, md5(password)]);
+    return rows[0];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { getUserByUsernameAndPassword };
